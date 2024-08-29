5d31b0cbac632c0fff49f29a55565c100406d286
Isuru Ranaweera <68464572+Isuru-rana@users.noreply.github.com>
2024-08-29 20:06:14 +0530

    2nd edition

---
+++ Blockchain/Backend/core/blockchain.py
@@ -0,0 +1,50 @@
+import sys
+sys.path.append('D:/project/Bitcoin/blockchains')
+
+from Blockchain.Backend.core.block import Block
+from Blockchain.Backend.core.blockheader import BlockHeader
+from Blockchain.Backend.util.util import hash256
+from Blockchain.Backend.core.database.database import BlockchainDB
+import time
+#import json
+
+ZERO_HASH = '0' * 64
+VERSION = 1
+
+class Blockchain:
+    def __init__(self):
+        self.chain = []
+        self.GenesisBlock()
+    
+    def write_on_disk(self, block):
+        blockchainDB = BlockchainDB()
+        blockchainDB.write(block)
+
+    def fetch_last_block(self):
+        blockchainDB = BlockchainDB()
+        return blockchainDB.lastBlock()
+
+    def GenesisBlock(self):
+        BlockHeight = 0
+        prevBlockHash = ZERO_HASH
+        self.addBlock(BlockHeight, prevBlockHash)
+
+    def addBlock(self, BlockHeight, prevBlockHash):
+        timestamp = int(time.time())
+        Transaction = f"Codies Alert sent {BlockHeight} Bitcoins to Jeo"
+        merkleRoot = hash256(Transaction.encode()).hex()
+        bits = 'ffff001f'
+        blockheader = BlockHeader(VERSION, prevBlockHash, merkleRoot, timestamp, bits)
+        blockheader.mine()
+        self.write_on_disk([Block(BlockHeight, 1, blockheader.__dict__, 1, Transaction).__dict__])
+
+    def main(self):
+        while True:
+            lastBlock = self.fetch_last_block()
+            BlockHeight = lastBlock["Height"] + 1
+            prevBlockHash = lastBlock['BlockHeader']['blockHash']
+            self.addBlock(BlockHeight, prevBlockHash)
+
+if __name__ == "__main__":
+    blockchain = Blockchain()
+    blockchain.main()
